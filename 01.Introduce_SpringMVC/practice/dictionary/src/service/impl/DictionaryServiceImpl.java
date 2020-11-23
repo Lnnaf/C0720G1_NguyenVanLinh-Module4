be9afe0287d0052f11fe4caf7d5fe3b0c266b72f
lnnaf <vanlinh12b5@gmail.com>
2020-11-23 08:11:42 +0700

    add bootstrap

--- 01.Introduce_SpringMVC/practice/dictionary/src/service/impl/DictionaryServiceImpl.java
+++ 01.Introduce_SpringMVC/practice/dictionary/src/service/impl/DictionaryServiceImpl.java
@@ -11,18 +11,18 @@ public class DictionaryServiceImpl implements IDictionaryService {
 
     @Override
     public String findWord(String keyWord) {
-        Map<String,String> list = new HashMap<>();
-        String result=null;
-        list.put("hello","Xin Chào");
-        list.put("Love","Tình Yêu");
-        list.put("Bike","Xe Máy");
-        list.put("Planet","Hành Tinh");
+        Map<String, String> list = new HashMap<>();
+        String result = null;
+        list.put("hello", "Xin Chào");
+        list.put("Love", "Tình Yêu");
+        list.put("Bike", "Xe Máy");
+        list.put("Planet", "Hành Tinh");
 
         for (Map.Entry<String, String> entry : list.entrySet()) {
-            if(keyWord.equals(entry.getKey())) {
+            if (keyWord.equals(entry.getKey())) {
                 result = list.get(keyWord);
                 break;
-            }else {
+            } else {
                 result = "Can't find anything like that";
             }
         }
